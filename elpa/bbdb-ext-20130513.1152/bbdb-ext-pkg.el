af79348f056b1b9ce760dcc3afa8ffd5c1ffab7c
Zhongnan Xu <zhongnan.xu@gmail.com>
2015-03-12 11:24:50 -0400

    Loads up jmax init

--- elpa/bbdb-ext-20130513.1152/bbdb-ext-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "bbdb-ext" "20130513.1152" "Extra commands for BBDB" (quote ((bbdb "2.36"))))
--- init.el
+++ init.el
@@ -15,7 +15,37 @@
 (add-to-list 'load-path zx-emacs-dir)
 (add-to-list 'load-path zx-user-dir)
 
-(require 'packages)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Load JMAX
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; This code loads up the part of jmax that I want -- mainly things to
+;; help with org-mode options
+(defvar starter-kit-dir (expand-file-name "jmax" zx-emacs-dir)
+  "jmax directory that contains elisp files that I might want to use")
+
+(defvar user-dir (expand-file-name "user" zx-emacs-dir)
+  "user directory for personal code")
+
+(add-to-list 'load-path starter-kit-dir)
+(add-to-list 'load-path user-dir)
+
+;; (load-file (expand-file-name "jorg-bib.el" jmax-dir))
+;; (load-file (expand-file-name "jmax.el" starter-kit-dir))
+(require 'init)
+
+;; These are customizations that we can add to jmax
+(setq reftex-default-bibliography '("~/Dropbox/bibliography/references.bib"))
+
+;; see org-ref.el for use of these variables
+(setq org-ref-bibliography-notes "~/Dropbox/bibliography/notes.org"
+      org-ref-default-bibliography '("~/Dropbox/bibliography/library.bib")
+      org-ref-pdf-directory "~/Dropbox/bibliography/bibtex-pdfs/")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Load my own code
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (require 'customization)
 (require 'zx-agenda)
 ;;; end init
