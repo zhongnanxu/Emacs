b9b08f474f764a61c8fc5fa2f471193b163fe2a4
Zhongnan Xu <zhongnan.xu@gmail.com>
2016-03-04 15:13:38 -0600

    Added TODO from meeting with Paul

--- zx-agenda.el
+++ zx-agenda.el
@@ -3,16 +3,13 @@
 ;; Add misc agenda files to the agenda
 (defvar misc-agenda (list (concat zx-user-dir "general.org")
 			  (concat zx-user-dir "papers.org")
-			  (concat zx-user-dir "exxon-mobil.org")
 			  (concat zx-user-dir "notebook.org")
+			  (concat zx-user-dir "lit-review.org")
 			  ))
 
 ;; Add active projects to the agenda
-(defvar act-proj (list (concat rdir "oxide-polymorphs-OER/oxide-polymorphs-OER.org")
-		       (concat rdir "NiO-adsorption/NiO-adsorption.org")
-		       (concat rdir "exxon-surfaces/exxon-surfaces.org")
-		       (concat rdir "oxide-interfaces/oxide-interfaces.org")
-		       (concat rdir "BaTiO3-TiO2-interface/BaTiO3-TiO2-interface.org")
+(defvar act-proj (list (concat rdir "ABO3-TiO2-interface/BaTiO3-TiO2-interface.org")
+		       (concat rdir "PtMo-nanoparticles/PtMo-nanoparticles.org")
 		       ))
 
 ;; (setq org-agenda-files (list (concat rdir "zhongnax/zhongnax.org")
@@ -21,8 +18,10 @@
 ;; (setq org-agenda-files
 ;;       (append (sa-find-org-file-recursively rdir)))
 
-(if (string= system-name "gilgamesh.cheme.cmu.edu")
-    (setq org-agenda-files (append misc-agenda act-proj))
-  (setq org-agenda-files (append misc-agenda)))
+;; (if (string= system-name "gilgamesh.cheme.cmu.edu")
+;;    (setq org-agenda-files (append misc-agenda act-proj))
+;;  (setq org-agenda-files (append misc-agenda)))
+
+(setq org-agenda-files (append misc-agenda act-proj))
 
 (provide 'zx-agenda)
