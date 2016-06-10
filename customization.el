28738464123fa7e5ebf169a6fd9f05631d476863
Zhongnan Xu <zhongnan.xu@gmail.com>
2016-06-10 14:57:41 -0500

    Added a function for deleting file and buffer

--- customization.el
+++ customization.el
@@ -152,4 +152,18 @@ If FILEXT is provided, return files with extension FILEXT instead."
 			  org-file-list) ; add files found to result
 	  (add-to-list 'org-file-list org-file)))))))
 
+(defun delete-file-and-buffer ()
+  "Kill the current buffer and deletes the file it is visiting."
+  (interactive)
+  (let ((filename (buffer-file-name)))
+    (when filename
+      (if (vc-backend filename)
+          (vc-delete-file filename)
+        (progn
+          (delete-file filename)
+          (message "Deleted file %s" filename)
+          (kill-buffer))))))
+
+(global-set-key (kbd "C-c D")  'delete-file-and-buffer)
+
 (provide 'customization)
